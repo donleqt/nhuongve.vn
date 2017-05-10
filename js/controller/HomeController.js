b74e00dd11906ce010d30f48e9a8ba5f7d709021
DESKTOP-Q6EAHGS\Don Le <donleqt@gmail.com>
2017-05-11 02:00:15 +0700

    setup angularjs

---
+++ js/controller/HomeController.js
@@ -0,0 +1,7 @@
+myApp.controller('SearchController', function ($scope, $state, $rootScope, $http) {
+    console.log('Xin chao tu PostTicketController');
+
+    /**
+     * Plain js and jquery function
+     */
+});
